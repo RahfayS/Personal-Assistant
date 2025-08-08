6ab79451997b3c118961d94e8684a13f380a25f0
Rahfay <saberirahfay@gmail.com>
2025-08-08 14:24:49 -0400

    chore: Only grant access to gestures if a user is found

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-import cv2
-from face_detection_utils.detection import *
-from face_detection_utils.get_photos import *
-from face_detection_utils.encodings import get_encodings
-from face_detection_utils.auto_login import auto_login
-from user_data_utils.registration import *
-from gesture_control.volume_controller import VolumeController
-from gesture_control.hand_tracker import TrackHands
-from gesture_control.gesture_utils import *
 from user_flow.auth import reg_user
 from gesture_control.gesture_main import gestures
+import pyttsx3
 
 def main():
-    reg_user()
-    gestures()
+    found, name = reg_user()
+    if found:
+        gestures()
 
 
 
