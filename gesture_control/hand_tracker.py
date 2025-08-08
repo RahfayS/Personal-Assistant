76d98733f56a44ea61d1c5c765f1ce2f36dc811b
Rahfay <saberirahfay@gmail.com>
2025-08-08 13:21:55 -0400

    Implement detect_hands method to process a frame, extract hand landmarks, and optionally draw landmarks on the frame

---
+++ gesture_control/hand_tracker.py
@@ -0,0 +1,48 @@
+import cv2
+import mediapipe as mp 
+import subprocess
+from .detection import Detection
+import math
+
+
+
+class TrackHands(Detection):
+
+    def __init__(self, mode = False, complexity = 1, min_detection_confidence = 0.7, min_tracking_confidence = 0.5, max_num_hands = 1):
+        super().__init__(mode, complexity, min_detection_confidence, min_tracking_confidence)
+        self.max_hands = max_num_hands
+
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+    static_image_mode=self.mode,
+    max_num_hands=self.max_hands,
+    model_complexity=self.complexity,
+    min_detection_confidence=self.min_detection_confidence,
+    min_tracking_confidence=self.min_tracking_confidence
+)
+
+        self.mp_drawing = mp.solutions.drawing_utils
+    
+    def detect_hands(self, frame, draw = True):
+
+        landmarks = []
+        
+        # Returns a results object containing information about landmarks (position coords)
+        results = self.hands.process(frame)
+
+        if results.multi_hand_landmarks:
+            # Iterate through all 21 landmarks for each hand detected
+            for hands in results.multi_hand_landmarks:
+                # Iterate through each landmark position getting their corresponding id
+                for id, lm in enumerate(hands.landmark):
+                    # get frame dimensions
+                    h, w, c = frame.shape
+
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+
+                    landmarks.append([id, cx, cy])
+
+                    if draw:
+                        self.mp_drawing.draw_landmarks(frame, hands, self.mp_hands.HAND_CONNECTIONS)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        return frame, landmarks
