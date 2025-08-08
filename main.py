561b4b6c6ac38e729e814cbc36f38a456f8efe37
Rahfay <saberirahfay@gmail.com>
2025-08-08 13:10:44 -0400

    Refactor: move face detection logic from main.py to separate module

--- main.py
+++ main.py
@@ -4,53 +4,16 @@ from face_detection_utils.get_photos import *
 from face_detection_utils.encodings import get_encodings
 from face_detection_utils.auto_login import auto_login
 from user_data_utils.registration import *
+from gesture_control.volume_controller import VolumeController
+from gesture_control.hand_tracker import TrackHands
+from gesture_control.gesture_utils import *
+from user_flow.auth import reg_user
+from gesture_control.gesture_main import gestures
 
 def main():
-    manager = userManager()
-    identified = auto_login(manager)
+    reg_user()
+    gestures()
 
-    if identified:
-        print('User identified')
-    else:
-        name, email = get_user_info()
-        # Load users from disk
-        users = manager.load_users()
-        print(users)
-
-        if email not in manager.email_to_id:
-            user = manager.register_user(name, email)
-
-            success = get_screenshots(name)
-            if not success:
-                print('[ERROR] Failed to capture screenshots')
-                return  
-            encoding = get_encodings(user.name)
-            if encoding is None:
-                print('[ERROR] failed to extract face encodings')
-                return
-            user.encoding = encoding
-            manager.save_users()
-        else:
-            user_id = manager.email_to_id[email]
-            user = manager.users[user_id]
-            print(f"[INFO] User {user.name} already registered.")
-
-        cap = cv2.VideoCapture(1)
-        count = 0
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-
-            output_frame = compare_faces(users, frame)
-            resized = cv2.resize(output_frame,(0,0), fx= 0.3, fy=0.3)
-            cv2.imshow('Face Recognition', resized)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        cap.release()
-        cv2.destroyAllWindows()
 
 
 
