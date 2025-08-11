89e724ea0871d892c0657a343b60413ecbe46b80
Rahfay <saberirahfay@gmail.com>
2025-08-11 16:49:59 -0400

    Removed frame resize from preprocess

--- frame_utils/preprocess.py
+++ frame_utils/preprocess.py
@@ -10,7 +10,6 @@ def preprocess(frame):
     if frame is not None and frame.shape[0] > 0 and frame.shape[1] > 0:
         frame = cv2.flip(frame, 1)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.resize(frame, (0,0), fx = 0.75, fy = 0.75)
     else:
         print('[WARN] Frame empty')
 
