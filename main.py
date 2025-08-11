d97974315060b68b15cf3b539721a3625b751317
Rahfay <saberirahfay@gmail.com>
2025-08-11 16:48:57 -0400

    Add case switches for each channel avaliable

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from user_flow.auth import reg_user
 from gesture_control.gesture_main import gestures
-import pyttsx3
+from get_mode.get_mode_channel import get_channel
 
 def main():
     found, name = reg_user()
     if found:
-        gestures()
-
+        channel = get_channel()
+        match channel:
+            case 'Idle':
+                print(f'CURRENT MODE: {channel}')
+            case 'Study':
+                print(f'CURRENT MODE: {channel}')
+                pass
+            case 'Media':
+                print(f'CURRENT MODE: {channel}')
+                gestures()
+                pass
 
 
 
