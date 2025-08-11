2a589cdc53299bb7d83a7e3d86bd7cffe49d028f
Rahfay <saberirahfay@gmail.com>
2025-08-11 17:02:40 -0400

    Add init for face_detection_utils

---
+++ face_detection_utils/__init__.py
@@ -0,0 +1,11 @@
+from .auto_login import *
+from .face_detection import *
+from .encodings import *
+from .get_photos import *
+
+__all__ = [
+    "auto_login",
+    "face_detection",
+    "encodings",
+    "get_photos",
+]
