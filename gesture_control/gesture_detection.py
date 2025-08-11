a8041a17fcc24706ebc171b95b405cbf138e8a04
Rahfay <saberirahfay@gmail.com>
2025-08-11 17:01:26 -0400

    Renamed to gesture detection

---
+++ gesture_control/gesture_detection.py
@@ -0,0 +1,9 @@
+import mediapipe as mp
+
+class Detection:
+
+    def __init__(self,mode = False, complexity = 1, min_detection_confidence = 0.7, min_tracking_confidence = 0.5):
+        self.mode = mode
+        self.complexity = complexity
+        self.min_detection_confidence = min_detection_confidence
+        self.min_tracking_confidence = min_tracking_confidence
