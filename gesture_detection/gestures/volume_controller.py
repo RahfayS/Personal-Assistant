f33b030c6e2a5a65f7506756c5d7804f8c73c7f5
Rahfay <saberirahfay@gmail.com>
2025-08-12 19:21:37 -0400

    Refactor: volume is now changed if a pinch is first detected

--- gesture_detection/gestures/volume_controller.py
+++ gesture_detection/gestures/volume_controller.py
@@ -2,69 +2,86 @@ import cv2
 import math
 import subprocess
 import time
+from collections import deque
 from frame_utils.draw_text import put_text_top_right
-
 class VolumeController:
-    def __init__(self):
+    PINCH_THRESHOLD = 50
+    MIN_DISTANCE = 50
+    MAX_DISTANCE = 450
+
+    def __init__(self, maxlen=50):
         self.last_vol = -1
-        self.last_update_time = 0
+        self.state = "WAITING_FOR_PINCH"  # or COLLECTING_FRAMES / WAITING_FOR_RELEASE
+        self.maxlen = maxlen
+        self.avg_len = deque(maxlen=maxlen)
+        self.last_pinch_time = 0
+        self.display_message = None
+        self.display_until = 0
 
-    def change_volume(self, frame,landmarks, draw = True):
+    def change_volume(self, frame, landmarks):
+        distance = self.get_distance(frame, landmarks)
 
-        lm_1, lm_2 = landmarks[4], landmarks[8]
+        if self.state == "WAITING_FOR_PINCH":
+            if distance < self.PINCH_THRESHOLD and (time.time() - self.last_pinch_time) > 0.5:
+                self.state = "COLLECTING_FRAMES"
+                self.avg_len.clear()
+                self.last_pinch_time = time.time()
+                self.show_msg('Raise fingers to Adjust Volume', duration=1)
 
-        # Get the coords of the landmarks representing the thumb and index finger
 
-        x1, y1 = lm_1[1], lm_1[2]
-        x2, y2 = lm_2[1], lm_2[2]
+        elif self.state == "COLLECTING_FRAMES":
+            self.avg_len.append(distance)
+            current_vol = self.get_current_volume(distance)
+            self.show_msg(f'CURRENT VOL:{int(current_vol)} %', duration=1)
+            if len(self.avg_len) >= self.maxlen:
+                recent_len = list(self.avg_len)[-20:] # Get the last 20 lengths in the deque
+                avg_len = sum(recent_len) / len(recent_len)
+                vol = self.set_volume(avg_len)
+                self.show_msg(f'VOLUME SET: {vol}', duration=3)
+                self.state = "WAITING_FOR_RELEASE"
 
-        # Get the cords of the center of the landmarks
-
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-
-        # Calculate the length between the circles/landmarks
-        length = math.hypot(x2 - x1, y2 - y1)
-
-        # Draw circles on the landmarks, then measure the length between the circles, which we will use to control the volume
-        if draw:
-            cv2.circle(frame, (x1,y1),10,(255,255,255), cv2.FILLED)
-            cv2.circle(frame, (x2,y2),10,(255,255,255), cv2.FILLED)
-            cv2.line(frame, (x1,y1), (x2,y2), (0,125,125), 2)
-            cv2.circle(frame, (cx,cy),10,(0,255,00), cv2.FILLED)
-
-            if length < 50:
-                cv2.circle(frame, (cx,cy),15, (0,0,255), cv2.FILLED)
-
-        vol = self.set_volume(length)
-
-        frame = put_text_top_right(frame, f'VOLUME: {vol}')
+        elif self.state == "WAITING_FOR_RELEASE":
+            if distance > self.PINCH_THRESHOLD:
+                print("PINCH RELEASED")
+                self.state = "WAITING_FOR_PINCH"
+        if self.display_message and time.time() < self.display_until:
+            frame = put_text_top_right(frame, self.display_message)
 
         return frame
 
+    def get_distance(self, frame, landmarks, draw=True):
+        lm_1, lm_2 = landmarks[4], landmarks[8]
+        _, x1, y1 = lm_1
+        _, x2, y2 = lm_2
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        distance = math.hypot(x2 - x1, y2 - y1)
+
+        if self.state == 'COLLECTING_FRAMES':
+            cv2.circle(frame, (x1, y1), 10, (255, 255, 255), cv2.FILLED)
+            cv2.circle(frame, (x2, y2), 10, (255, 255, 255), cv2.FILLED)
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 125, 125), 2)
+            cv2.circle(frame, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
+            if distance < self.MIN_DISTANCE:
+                cv2.circle(frame, (cx, cy), 10, (0, 0, 255), cv2.FILLED)
+                
+        return distance
+
+    def get_current_volume(self,length):
+
+        length = 0 if length < self.MIN_DISTANCE else min(length,self.MAX_DISTANCE)
+        vol_percent = (length / self.MAX_DISTANCE) * 100
+
+        return vol_percent
 
     def set_volume(self, length):
 
-        # The MAX and min distance we will consider for volume changes
-        MAX = 450
-
-        MIN = 50
-
-        if length > MAX:
-            length = MAX
-        elif length < MIN:
-            length = 0
-        
-        vol_percent = (length / MAX) * 100
-
-        curr_time = time.time()
-        # update volume only if volume changed significantly (2%)
-        if abs(vol_percent - self.last_vol) >= 0.5:
-            # If length remain consistant for 3 seconds then change volume
-            if abs(length - 30) > 10 and curr_time - self.last_update_time >= 3:
-                subprocess.run(["osascript", "-e", f"set volume output volume {vol_percent}"])
-                self.last_vol = vol_percent
-                self.last_update_time = curr_time
-
-        
+        length = 0 if length < self.MIN_DISTANCE else min(length,self.MAX_DISTANCE)
+        vol_percent = (length / self.MAX_DISTANCE) * 100
+        subprocess.run(["osascript", "-e", f"set volume output volume {vol_percent}"])
+        self.last_vol = vol_percent
         return int(self.last_vol)
 
+    def show_msg(self, message, duration = 2):
+        self.display_message = message
+        self.display_until = time.time() + duration
