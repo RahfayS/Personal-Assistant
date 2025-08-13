3fdcf3e2ece790438678d010fcad759e6dc233f1
Rahfay <saberirahfay@gmail.com>
2025-08-13 12:01:13 -0400

    Reconfigure base to function as the gesture hub for the media channel

--- gesture_detection/media_channel.py
+++ gesture_detection/media_channel.py
@@ -1,19 +1,28 @@
 import cv2
-from face_detection_utils import face_detection, get_photos
 from .gestures.volume_controller import VolumeController
 from .gestures.hand_tracker import TrackHands
 from .gestures.closed_fist import ClosedFist
 from frame_utils.preprocess import *
 from frame_utils.draw_text import *
+from speech_commands.media_commands import MediaCommands
+import threading
 
-def gestures():
+def media_channel():
+
     vc = VolumeController()
     hand_tracker = TrackHands()
     fist_tracker = ClosedFist()
+    media_speech = MediaCommands()
+
+    def listen_for_speech():
+        while True:
+            media_speech.media_commands()
+            time.sleep(0.5)
+    
+    threading.Thread(target=listen_for_speech,daemon=True).start()
 
     prev_time = 0
     cap = cv2.VideoCapture(1)
-
 
     while True:
         ret, frame = cap.read()
@@ -26,16 +35,15 @@ def gestures():
 
         # Start with the frame to display
         display_frame = frame_detect
-
+        
         if len(landmarks) != 0:
             # Control volume gestures
             display_frame = vc.change_volume(display_frame,landmarks)
             # Gesture to close application
-            isClosed = fist_tracker.close_app(display_frame,landmarks,hand_label)
+            isPaused,app = fist_tracker.closed_fist(display_frame,landmarks,hand_label)
 
-            if isClosed:
-                print('CLOSE APP')
-                break
+            if isPaused and app is not None:
+                print(f'{app} Paused')
 
         # Calculate FPS
         fps, prev_time = get_fps(prev_time)
