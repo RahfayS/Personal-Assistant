bc4c2d5b06411fb33c41ca1e8f7f41a3f8804904
Rahfay <saberirahfay@gmail.com>
2025-08-18 11:22:05 -0400

    Add function to skip left or right if a swipe is detected

--- media/playback_control.py
+++ media/playback_control.py
@@ -1,4 +1,5 @@
 import subprocess
+import pyautogui
 
 def toggle_play_pause():
     '''
@@ -10,3 +11,10 @@ def toggle_play_pause():
     tell application "System Events" to keystroke " "  -- spacebar
     '''
     subprocess.call(['osascript', '-e', script])
+
+
+def youtube_skip(hand_label):
+    if hand_label == 'Left':  # swipe right
+        pyautogui.press('right')  # skip forward 5s
+    elif hand_label == 'Right':  # swipe left
+        pyautogui.press('left')   # skip backward 5s
