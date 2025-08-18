d69f9ecc7dd17508e8ed03a3225a171382b97fa3
Rahfay <saberirahfay@gmail.com>
2025-08-18 11:22:39 -0400

    Add get context to retrieve first most tab in users browser

--- media/media_main.py
+++ media/media_main.py
@@ -6,7 +6,8 @@ from gestures.base import TrackHands
 from gestures.detect_fist import DetectFist
 from gestures.detect_palm import DetectPalm
 from gestures.detect_pose import DetectPose
-from .playback_control import toggle_play_pause
+from gestures.detect_swipes import DetectSwipes
+from .playback_control import toggle_play_pause,youtube_skip
 from utils.preprocess import *
 from utils.draw_text import *
 from speech.media_commands import MediaCommands
@@ -18,24 +19,41 @@ def media_main():
     media_speech = MediaCommands()
     palm = DetectPalm()
     hand_tracker = TrackHands()
-
+    swipe = DetectSwipes()
     pose = DetectPose()
     mute_control = MuteControl()
 
     last_trigger_time = 0
+
+    last_call = 0
+    
     off_screen = False
 
 
     FIRST_IGNORE_TIME = 1
 
+    
     '''
     def listen_for_speech():
         while True:
             media_speech.media_commands()
-            time.sleep(0.5)
-    
+            time.sleep(1)
+
     threading.Thread(target=listen_for_speech,daemon=True).start()
+
     '''
+
+    def get_context():
+        app = media_speech.get_active_app()
+        if 'spotify' in app:
+            context = 'spotify'
+        elif 'youtube' in app:
+            context = 'youtube'
+        else:
+            context = 'generic'
+
+        return context
+    
     prev_time = 0
     cap = cv2.VideoCapture(1)
 
@@ -59,7 +77,6 @@ def media_main():
 
         # Detect a fist, if a fist is detected break from webcam
         closed_fist, fist_detected = fist_tracker.is_fist_closed(display_frame,hand_landmarks,hand_label)
-
         if closed_fist:
             break
 
@@ -85,7 +102,17 @@ def media_main():
                 toggle_play_pause()
             off_screen = False
 
+        if time.time() - last_call >= 10:
+            context = get_context()
+            last_call = time.time()
+            print(context)
 
+        if context == 'youtube':
+            swipe_detected,dx, hand_label = swipe.detect_swipe(frame,hand_landmarks,hand_label)
+            if swipe_detected:
+                print('SWIPE DETECTED')
+                youtube_skip(hand_label)
+                
         mute_control.mute_app(display_frame, hand_landmarks, pose_landmarks,draw=False)
 
         # Calculate FPS and display
