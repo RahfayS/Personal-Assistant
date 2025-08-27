6030f1677f9f5b4414896ea5fe619dbd6f70c726
Rahfay <saberirahfay@gmail.com>
2025-08-26 22:40:10 -0400

    Refactor toggle_play_pause: Enhance Spotify playback control and maintain Chrome pause functionality

--- media/playback_control.py
+++ media/playback_control.py
@@ -1,20 +1,40 @@
 import subprocess
 import pyautogui
-
-def toggle_play_pause():
-    '''
-    Pauses any google chrome application
-    '''
-    script = '''
-    tell application "Google Chrome" to activate
-    delay 0.1
-    tell application "System Events" to keystroke " "  -- spacebar
-    '''
-    subprocess.call(['osascript', '-e', script])
+from spotipy.exceptions import SpotifyException
 
 
-def youtube_skip(hand_label):
-    if hand_label == 'Left':  # swipe right
-        pyautogui.press('right')  # skip forward 5s
-    elif hand_label == 'Right':  # swipe left
-        pyautogui.press('left')   # skip backward 5s
+
+def toggle_play_pause(media_manager):
+
+    context = media_manager.mode
+    if context == 'spotify':
+
+        devices = media_manager.spotify_manager.sp.devices()['devices']
+        active_devices = [d for d in devices if d['is_active']]
+
+        if active_devices:
+            device_id = active_devices[0]['id']
+            try:
+                state = media_manager.spotify_manager.sp.current_playback()
+                if state and state['is_playing']:
+                    media_manager.spotify_manager.sp.pause_playback(device_id=device_id)
+                else:
+                    media_manager.spotify_manager.sp.start_playback(device_id=device_id)
+            except SpotifyException as e:
+                print(f"Error pausing Spotify: {e}")
+        else:
+            print("No active Spotify devices to control.")
+
+    elif context == 'youtube':
+        pass
+    else:
+
+        '''
+        Pauses any google chrome application
+        '''
+        script = '''
+        tell application "Google Chrome" to activate
+        delay 0.1
+        tell application "System Events" to keystroke " "  -- spacebar
+        '''
+        subprocess.call(['osascript', '-e', script])
